0ba140c2523520e57605874728b23dfefbb4be2b
Joshua Philips <jmichaelphilips@gmail.com>
2016-10-19 17:25:41 -0500

    completed the first practice problem on page 37 of programming elixir

--- fizzbuzz.exs
+++ fizzbuzz.exs
@@ -1,5 +1,18 @@
-def hello do
-    IO.puts "Hello"
+# Programming Elixir
+# Dave Thomas
+# Chapter 5
+# Page 37
+# Exercise no. 2
+# Fizzbuzz 
+
+fizzbuzz = fn 
+    { 0, 0, _ } -> "Fizzbuzz"
+    { 0, _, _ } -> "Fizz"
+    { _, 0, _ } -> "Buzz"
+    {_, _, c} -> "#{c}"
 end
 
-hello.()
+IO.puts fizzbuzz.({0, 0, "hey"})
+IO.puts fizzbuzz.({0, 3, "heyo"})
+IO.puts fizzbuzz.({"hey", 0, 0})
+IO.puts fizzbuzz.({"hey", "hey", "hey"})
