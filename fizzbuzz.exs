ae0215eab9344d52c0656a2e1983ced896b7cd94
Joshua Philips <jmichaelphilips@gmail.com>
2016-10-19 17:56:14 -0500

    Added a new function to make it more fizzbuzzy

--- fizzbuzz.exs
+++ fizzbuzz.exs
@@ -12,7 +12,18 @@ fizzbuzz = fn
     {_, _, c} -> "#{c}"
 end
 
-IO.puts fizzbuzz.({0, 0, "hey"})
-IO.puts fizzbuzz.({0, 3, "heyo"})
-IO.puts fizzbuzz.({"hey", 0, 0})
-IO.puts fizzbuzz.({"hey", "hey", "hey"})
+single_number = fn (num) -> fizzbuzz.({rem(num, 3), rem(num, 5), num}) end
+
+# IO.puts fizzbuzz.({0, 0, "hey"})
+# IO.puts fizzbuzz.({0, 3, "heyo"})
+# IO.puts fizzbuzz.({"hey", 0, 0})
+# IO.puts fizzbuzz.({"hey", "hey", "hey"})
+
+IO.puts single_number.(10)
+IO.puts single_number.(11)
+IO.puts single_number.(12)
+IO.puts single_number.(13)
+IO.puts single_number.(14)
+IO.puts single_number.(15)
+IO.puts single_number.(16)
+IO.puts single_number.(17)
