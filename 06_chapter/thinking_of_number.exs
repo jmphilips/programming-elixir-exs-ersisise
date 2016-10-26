7e3b2b92be30e1b9e51c1165a9d7c2153665306d
Joshua Philips <jmichaelphilips@gmail.com>
2016-10-25 21:55:40 -0500

    Working through the problem

--- 06_chapter/thinking_of_number.exs
+++ 06_chapter/thinking_of_number.exs
@@ -1,3 +1,16 @@
-defmodule thinking_of_number do
+defmodule NumberChop do
     
+    def chop(n, rang) do
+        midway = round(rang.last / 2)
+        IO.puts "Is it #{midway}"
+
+        chop(n, midway..midway * 2)
+    end
+
 end
+   
+
+
+
+
+
